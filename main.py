9ae20bd45cf8337cd00183f890c963cc331e262f
Anton Chepaldin <anton.chepaldin@astrazeneca.com>
2021-03-11 17:05:33 +0000

    begenning done

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-from assets import UI, main_player
+from assets import UI, main_player, save_info
 from dead import dead
+from holy import holy
 
 
-def move_to_location(Player):
-    location = UI.options("(1-4)", 1, 3)
+class main():
+    def move_to_location(Player):
+        print("would you like to travel toh 1:hell or heaven?")
+        location = UI.options("(1-2)", 1, 2)
 
-    if location == 1:
-        dead.spawn(Player)
+        if location == 1:
+            dead.spawn(Player)
 
-    if location == 2:
-        print("bulding it!")
-        # holy.spawn(Player)
+        if location == 2:
+            holy.spawn(Player)
 
-    if location == 3:
-        # monster.spawn(Player)
-        print('bulding it')
+    def on_boarding():
+        print(f"hello and welcome to my game!\n What is your name?")
+        name = UI.ask("(abc)")
+        print(f"Oh your name is {name} and what is your gender?")
+        gender = UI.ask("(abc)")
+        Player = main_player(name, gender)
+        print(f'here is your stats:\n {Player.stats()}')
+        print("""Sorry for i was only interseted, it's hard to see.
+        Welcome to this wonderful land.""")
+        save_info.write(Player,Player.name)
+        main.move_to_location(Player)
+
+    def reload():
+        print('what was your character called?')
+        while True:
+            try:
+                player = save_info.read(main_player("name","gender"),input("(abc)>>>"))
+                break
+            except FileNotFoundError:
+                print('that is wrong which one is it?')
+                continue
+        print(f'here is your stats:\n {player.stats()}\n\nWelcome to this wonderful land.')
+        main.move_to_location(player)
 
 
-def on_boarding():
-    print(f"hello and welcome to my game!\n What is your name?")
-    name = UI.ask("(abc)")
-    print(f"Oh your name is {name} and what is your gender?")
-    gender = UI.ask("(abc)")
-    Player = main_player(name, gender)
-    print(f'here is your stats:\n {Player.stats()}')
-    print("""Sorry for i was only interseted, it's hard to see.
-    Welcome to this wonderful land.""")
-    move_to_location(Player)
+    def run():
+        print("Have you ever played before? \n 1: Yes, 2: No")
+        c = UI.options("(1-2)",1,2)
+        if c == 1:
+            main.reload()
 
-on_boarding()
+        if c == 2:
+            main.on_boarding()
+
+
+if __name__ == '__main__':
+    main.run()
